2caeb8e08f35d0e77d0e7d84e38b428e91eb5f47
Darkhan <darkhan_erdawletov@student.itpu.uz>
2023-10-18 15:04:01 +0500

    done!

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -12,7 +12,10 @@ public class QuadraticEquation {
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
 
-
+        double D = (b * b) - (4 * a * c);
+        if (D < 0) System.out.println("no roots");
+        else if (D == 0) System.out.println((-b + Math.sqrt(D)) / (2 * a));
+        else System.out.printf("%f %f", (-b + Math.sqrt(D)) / (2 * a), (-b - Math.sqrt(D)) / (2 * a));
 
     }
 
